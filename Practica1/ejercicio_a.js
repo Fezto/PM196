1f0dae942d58b52a4821c2f277438ce4e7ac5cff
Fezto <ayrtonsepch@gmail.com>
2025-05-16 15:38:52 -0600

    Práctica 1, Ejercicio C

--- Practica1/ejercicio_a.js
+++ Practica1/ejercicio_a.js
@@ -8,3 +8,5 @@ nombre = "Ana María";
 const saludo = `Hola, ${nombre}. Tienes ${edad} años!`;
 
 console.log(saludo)
+
+
--- Practica1/ejercicio_b.js
+++ Practica1/ejercicio_b.js
@@ -6,5 +6,3 @@ console.log(cuadrado(2));
 console.log(cuadrado(5));
 console.log(cuadrado(-3));
 
-const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años!`;
-console.log(saludoPersonalizado("Ayrton", "20"));
