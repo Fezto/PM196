ae6151dd7dfeda3cac3dcffe28f9f098a049be8b
Fezto <ayrtonsepch@gmail.com>
2025-05-16 15:22:55 -0600

    Práctica 1, Ejercicio B

---
+++ Practica1/ejercicio_b.js
@@ -0,0 +1,10 @@
+// * Ejercicio B.
+
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(2));
+console.log(cuadrado(5));
+console.log(cuadrado(-3));
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años!`;
+console.log(saludoPersonalizado("Ayrton", "20"));
