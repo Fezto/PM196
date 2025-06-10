a9d1c02cbac403e26d1e192ddb85c11313a2a652
Fezto <ayrtonsepch@gmail.com>
2025-06-10 15:38:56 -0600

    Práctica 4 UPDATE: Hola React Native

--- Practica4B/App.js
+++ Practica4B/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto = () => {
+  return (
+    <Text> Hola react native! </Text>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+
+    <View style={styles.container} >
       <StatusBar style="auto" />
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Button title='PRESIONAME' />
     </View>
   );
 }
