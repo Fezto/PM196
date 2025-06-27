5ff943fdde95b7743f784f1d3fd2989d77ab943b
Fezto <ayrtonsepch@gmail.com>
2025-06-27 14:40:36 -0600

    Práctica 10: <ActivityIndicator> incluido!

--- Practica10/App.js
+++ Practica10/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import {
   View,
   StyleSheet,
@@ -8,6 +9,21 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.splash}>
+        <Text style={styles.splashText}>Cargando...</Text>
+        <ActivityIndicator size="large" color="#FFFFFF" />
+      </View>
+    );
+  }
+
   return (
     <ImageBackground
       source={{
@@ -24,6 +40,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  splash: {
+    flex: 1,
+    backgroundColor: "#2C3E50",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  splashText: {
+    color: "white",
+    fontSize: 28,
+    marginBottom: 20,
+  },
   background: {
     flex: 1,
     justifyContent: "center",
